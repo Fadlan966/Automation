8e2c4393e505a74f0faa4ec8f4777b923fab54e8
Fadlan966 <muhmmdfdln@gmail.com>
2025-10-05 19:59:03 +0700

    Automation Testing

---
+++ Qbook Automation/loginTest.py
@@ -0,0 +1,35 @@
+import time
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    
+    # Buka halaman login
+    page.goto("https://cms-staging.qbookapp.com/login")
+
+    # Isi email dan password
+    page.get_by_role("textbox", name="Email").click()
+    page.get_by_role("textbox", name="Email").fill("muhmmdfdln@gmail.com")
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill("Qbook@2025")
+
+    # Klik tombol Sign In
+    page.get_by_role("button", name="Sign In").click()
+
+    # Tunggu beberapa detik agar login selesai & dashboard termuat
+    page.wait_for_timeout(15000) # Tunggu 15 detik
+
+    # Klik profil dan logout
+    page.get_by_text("Fadlan QA", exact=True).click()
+    page.get_by_role("img", name="down").locator("svg").click()
+    page.get_by_text("Logout").click()
+
+    # Tutup browser
+    context.close()
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
---
+++ test.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+def run_playwright():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://www.google.com")
+        browser.close()
+
+if __name__ == "__main__":
+    run_playwright()
