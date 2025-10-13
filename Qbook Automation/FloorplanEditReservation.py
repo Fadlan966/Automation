6a66f671506ca029ad8077e01d5bbbe458bc5384
Fadlan966 <muhmmdfdln@gmail.com>
2025-10-13 17:24:31 +0700

    Add & Edit Resrevation floorplan

---
+++ Qbook Automation/FloorplanEditReservation.py
@@ -0,0 +1,56 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://cms-staging.qbookapp.com/login")
+    page.get_by_role("textbox", name="Email").click()
+    page.get_by_role("textbox", name="Email").fill("muhmmdfdln@gmail.com")
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill("Qbook@2025")
+    page.get_by_role("button", name="Sign In").click()
+    page.get_by_role("complementary").locator("div").filter(has_text="Main AppDashboardRESERVATIONFloor PlanReservation ListScheduleTime").get_by_role("button").click()
+    page.get_by_role("link", name="Floor Plan").click()
+    page.goto("https://cms-staging.qbookapp.com/reservation-floor-plan?limit=99999&offset=0&reservationStartAtStart=2025-10-12T17%3A00%3A00.000Z&reservationStartAtEnd=2025-10-13T16%3A59%3A59.999Z&brandId=2019646b-f489-4355-b8ce-e32c090b9e2b&storeId=6a2cdb92-0b72-4083-8c5c-4510b3fafb2e")
+    page.get_by_text("BAKU, SCBD").click()
+    page.get_by_text("KN Testing").click()
+    page.get_by_role("cell", name="4:45 PM").dblclick()
+    page.get_by_role("button", name="Edit Reservation").click()
+    page.get_by_role("textbox", name="Input party name").click()
+    page.get_by_role("textbox", name="Input party name").fill("ITC")
+    page.locator("div").filter(has_text=re.compile(r"^2 selections$")).nth(1).click()
+    page.get_by_role("button", name="down Smoking Preferences").click()
+    page.get_by_text("PreviewSmoking").click()
+    page.get_by_role("button", name="up Smoking Preferences").click()
+    page.get_by_role("button", name="Save 2 Preferences").click()
+    page.locator("div").filter(has_text=re.compile(r"^Menu Packages$")).nth(1).click()
+    page.locator(".ant-checkbox-input").first.check()
+    page.locator(".ant-row > div:nth-child(3) > .ant-btn").first.click()
+    page.get_by_role("button", name="OK").click()
+    page.get_by_text("Mall", exact=True).click()
+    page.get_by_title("Fam").locator("div").click()
+    page.get_by_text("Direct").click()
+    page.get_by_title("Dinning City").locator("div").click()
+    page.get_by_role("button", name="Edit Memo").click()
+    page.get_by_role("textbox", name="Input Internal Reservation").click()
+    page.get_by_role("button", name="Customer Spend :").click()
+    page.get_by_role("textbox", name="Input Internal Reservation").click()
+    page.get_by_role("textbox", name="Input Internal Reservation").fill("Customer Spend : 1.400.000")
+    page.get_by_label("Edit Internal Reservation Memo").get_by_role("button", name="Save").click()
+    page.get_by_text("ACCEPTED").click()
+    page.get_by_text("CONFIRMED").click()
+    page.get_by_role("button", name="Save").click()
+    page.get_by_role("button", name="FadlanKuy").click()
+    page.get_by_text("FAFadlan QASuper Admin (3)").click()
+    page.get_by_text("Logout").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
